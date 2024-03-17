c082d81abdacfa123d1b09b3748fb7149cb6147c
Pramod Kumar Maurya <pramod.maurya12321@gmail.com>
2024-03-17 23:24:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+ function calculateAge() {
+            let dob = new Date(document.getElementById("birthday").value);
+            let currentDate = new Date();
+            let age = currentDate.getFullYear() - dob.getFullYear();
 
+            // Adjust age if the birthday hasn't occurred yet this year
+            if (currentDate.getMonth() < dob.getMonth() || (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
+                age--;
+            }
+
+            document.getElementById("result").innerHTML = `You are ${age} years old`;
